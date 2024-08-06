00410b76b0f4694ef12513122adf13027e7972d6
Hitesh Rawal <hiteshrawal600@gmail.com>
2024-08-06 23:42:58 +0530

    Event Module

---
+++ EventMod/index.js
@@ -0,0 +1,15 @@
+/* Event Module
+node.js has a built-in module,called "Events"
+where you can create-, fire-, and listen for- your own events.
+ example 1 - Registering for the event to be fired pnly one time using onece.
+ example 2 - create an event emmiter instance and register a couple of callbacks
+ example 3 - Registering for the event with calback parameters*/
+
+const  EventEMitter = require("events");  //where EventEmitter is class (captial E)
+
+const event = new EventEMitter();
+event.on("SayMyName", ()=> {
+    console.log("Your Name is Hitesh");
+});
+
+event.emit("SayMyName")
